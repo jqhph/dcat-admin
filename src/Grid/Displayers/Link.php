4f8a10aba718ae4561e2dedee4b3494fd63a61ea
Jiang qinghua <841324345@qq.com>
2019-07-09 15:59:55 +0800

    grid displayer link

--- src/Grid/Displayers/Link.php
+++ src/Grid/Displayers/Link.php
@@ -6,8 +6,17 @@ class Link extends AbstractDisplayer
 {
     public function display($href = '', $target = '_blank')
     {
-        $href = $href ?: $this->value;
+        if ($href instanceof \Closure) {
+            $href = $href->bindTo($this->row);
+
+            $href = call_user_func($href, $this->value);
+
+        } else {
+            $href = $href ?: $this->value;
+
+        }
 
         return "<a href='$href' target='$target'>{$this->value}</a>";
     }
+    
 }
