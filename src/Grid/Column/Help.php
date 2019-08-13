38eab918b27b0879f306d3fc53cce1d7d911f40b
jqh <841324345@qq.com>
2019-08-13 09:45:45 +0800

    修复表格列无法设置多个help问题

--- src/Grid/Column/Help.php
+++ src/Grid/Column/Help.php
@@ -4,6 +4,7 @@ namespace Dcat\Admin\Grid\Column;
 
 use Dcat\Admin\Widgets\Tooltip;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Str;
 
 class Help implements Renderable
 {
@@ -35,18 +36,20 @@ class Help implements Renderable
      */
     public function render()
     {
-        $tooltip = Tooltip::make('.grid-column-help');
+        $random = Str::random(8);
+
+        $tooltip = Tooltip::make('.grid-column-help-'.$random);
 
         if ($this->style && method_exists($tooltip, $this->style)) {
             $tooltip->{$this->style};
         }
 
         $tooltip->content($this->message)
-            ->top()
+            ->bottom()
             ->render();
 
         return <<<HELP
-<a href="javascript:void(0);" class="grid-column-help" >
+<a href="javascript:void(0);" class="grid-column-help-{$random}" >
     <i class="fa fa-question-circle"></i>
 </a>
 HELP;
