90a3b3c62cd54b234e05e1a5b6c0141af49c9449
jqh <841324345@qq.com>
2019-08-16 15:20:45 +0800

    icon

--- src/Form/Field/Icon.php
+++ src/Form/Field/Icon.php
@@ -6,8 +6,6 @@ use Dcat\Admin\Admin;
 
 class Icon extends Text
 {
-    protected $default = null;
-
     public function render()
     {
         $this->script = <<<JS
@@ -15,10 +13,7 @@ setTimeout(function () {
     $('{$this->getElementClassSelector()}').iconpicker({placement:'bottomLeft'});
 }, 10);
 JS;
-        if (is_null($this->default)) {
-            $this->default('');
-        }
- 
+
         $this->defaultAttribute('style', 'width: 200px');
 
         return parent::render();
