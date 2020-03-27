1ca0b000723152322906ad872346d62d82ec0610
jqh <841324345@qq.com>
2020-03-27 13:51:06 +0800

    travis run dusk

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -84,6 +84,7 @@ abstract class DuskTestCase extends BaseTestCase
             '--disable-gpu',
             '--headless',
             '--window-size=1920,1080',
+            '--whitelisted-ips',
         ]);
 
         return RemoteWebDriver::create(
