5ede1cfbc77e58e4609faefa806d81145048dc96
jqh <841324345@qq.com>
2020-03-27 12:48:27 +0800

    travis run dusk

--- tests/ChromeProcess.php
+++ tests/ChromeProcess.php
@@ -12,14 +12,15 @@ class ChromeProcess extends BaseChromeProcess
 
         if ($this->onWindows()) {
             $this->driver = realpath(__DIR__.'/resources/drivers/chromedriver-win.exe');
-        } elseif ($this->onMac()) {
-            $this->driver = realpath(__DIR__.'/resources/drivers/chromedriver-mac');
-        } else {
-            if (is_file('/usr/bin/google-chrome')) {
-                $this->driver = '/usr/bin/google-chrome';
-            } else {
-                $this->driver = realpath(__DIR__.'/resources/drivers/chromedriver-linux');
-            }
         }
+        //elseif ($this->onMac()) {
+        //    $this->driver = realpath(__DIR__.'/resources/drivers/chromedriver-mac');
+        //} else {
+        //    if (is_file('/usr/bin/google-chrome')) {
+        //        $this->driver = '/usr/bin/google-chrome';
+        //    } else {
+        //        $this->driver = realpath(__DIR__.'/resources/drivers/chromedriver-linux');
+        //    }
+        //}
     }
 }
