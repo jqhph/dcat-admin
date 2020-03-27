64fe9816f453c1c2bebd51341f442c33be5f744c
jqh <841324345@qq.com>
2020-03-27 10:18:34 +0800

    travis run dusk

---
+++ tests/resources/stubs/ComposerConfigCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Console\Command;
+
+class ComposerConfigCommand extends Command
+{
+    protected $signature = 'admin:composer-config';
+
+    public function handle()
+    {
+        $composer = base_path('composer.json');
+
+        /* @var \Illuminate\Filesystem\Filesystem $files */
+        $files = app('files');
+
+        $contents = json_decode($files->get($composer), true);
+
+        $contents['repositories'] = [
+            [
+                'type' => 'path',
+                'url'  => '../',
+            ],
+        ];
+
+        $files->put($composer, str_replace('\\/', '/', json_encode($contents)));
+    }
+}
