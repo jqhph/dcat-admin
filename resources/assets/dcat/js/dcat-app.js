73c7f355fb2659fb21f594001068ee0c510c3f56
jqh <841324345@qq.com>
2020-03-28 13:37:59 +0800

    .content滚动条优化

--- resources/assets/dcat/js/dcat-app.js
+++ resources/assets/dcat/js/dcat-app.js
@@ -88,7 +88,7 @@ function listen(Dcat) {
         Dcat.NP.configure({parent: '.app-content'});
 
         // 滚动条优化
-        // new PerfectScrollbar('html');
+        new PerfectScrollbar('.app-content.content');
 
         // layer弹窗设置
         layer.config({maxmin: true, moveOut: true, shade: false});
