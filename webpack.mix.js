aa4b6d7236feb03cf69936fea64ca9b2c7e29ebd
jqh <841324345@qq.com>
2020-03-31 12:18:07 +0800

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,12 +19,6 @@ const path = require('path')
 let distPath = mix.inProduction() ? 'resources/dist' : 'resources/pre-dist';
 
 
-/*
- |--------------------------------------------------------------------------
- | Vendor assets
- |--------------------------------------------------------------------------
- */
-
 function mixAssetsDir(query, cb) {
   (glob.sync('resources/assets/' + query) || []).forEach(f => {
     f = f.replace(/[\\\/]+/g, '/');
@@ -36,20 +30,6 @@ const sassOptions = {
   precision: 5
 };
 
-// plugins Core stylesheets
-mixAssetsDir('sass/plugins/**/!(_)*.scss', (src, dest) => mix.sass(src, dest.replace(/(\\|\/)sass(\\|\/)/, '$1css$2').replace(/\.scss$/, '.css'), sassOptions));
-
-// themes Core stylesheets
-mixAssetsDir('sass/themes/**/!(_)*.scss', (src, dest) => mix.sass(src, dest.replace(/(\\|\/)sass(\\|\/)/, '$1css$2').replace(/\.scss$/, '.css'), sassOptions));
-
-// pages Core stylesheets
-mixAssetsDir('sass/pages/**/!(_)*.scss', (src, dest) => mix.sass(src, dest.replace(/(\\|\/)sass(\\|\/)/, '$1css$2').replace(/\.scss$/, '.css'), sassOptions));
-
-// Core stylesheets
-mixAssetsDir('sass/core/**/!(_)*.scss', (src, dest) => mix.sass(src, dest.replace(/(\\|\/)sass(\\|\/)/, '$1css$2').replace(/\.scss$/, '.css'), sassOptions));
-
-// script js
-mixAssetsDir('js/scripts/**/*.js', (src, dest) => mix.scripts(src, dest));
 
 /*
  |--------------------------------------------------------------------------
@@ -57,12 +37,8 @@ mixAssetsDir('js/scripts/**/*.js', (src, dest) => mix.scripts(src, dest));
  |--------------------------------------------------------------------------
  */
 
-mixAssetsDir('assets/vendors/js/**/*.js', (src, dest) => mix.scripts(src, dest));
-mixAssetsDir('assets/vendors/css/**/*.css', (src, dest) => mix.copy(src, dest));
-mixAssetsDir('assets/vendors/css/editors/quill/fonts/', (src, dest) => mix.copy(src, dest));
 mix.copyDirectory('resources/assets/images', distPath + '/images');
 mix.copyDirectory('resources/assets/fonts', distPath + '/fonts');
-mix.copyDirectory('resources/assets/vendors', distPath + '/vendors');
 
 
 
@@ -86,25 +62,4 @@ mix.sass(dcatPath('sass/dcat-app.scss'), dcatDistPath('css/dcat-app.css'));
 mixAssetsDir('dcat/extra/*.js', (src, dest) => mix.js(src, dest));
 mixAssetsDir('dcat/extra/*.scss', (src, dest) => mix.sass(src, dest.replace('scss', 'css')));
 
-// ------------------------------------ Dcat Admin -------------------------------------------
 
-mix.js('resources/assets/js/core/app-menu.js', distPath + '/js/core')
-    .js('resources/assets/js/core/app.js', distPath + '/js/core')
-    .sass('resources/assets/sass/bootstrap.scss', distPath + '/css')
-    .sass('resources/assets/sass/bootstrap-extended.scss', distPath + '/css')
-    .sass('resources/assets/sass/colors.scss', distPath + '/css')
-    .sass('resources/assets/sass/components.scss', distPath + '/css')
-    .sass('resources/assets/sass/custom-rtl.scss', distPath + '/css')
-    .sass('resources/assets/sass/custom-laravel.scss', distPath + '/css');
-
-mix.then(() => {
-  if (process.env.MIX_CONTENT_DIRECTION === "rtl") {
-    let command = `node ${path.resolve('node_modules/rtlcss/bin/rtlcss.js')} -d -e ".css" ./${distPath}/css/ ./${distPath}/css/`;
-    exec(command, function (err, stdout, stderr) {
-      if (err !== null) {
-        console.log(err);
-      }
-    });
-    // exec(`./node_modules/rtlcss/bin/rtlcss.js -d -e ".css" ./${distPath}/css/ ./${distPath}/css/`);
-  }
-});
