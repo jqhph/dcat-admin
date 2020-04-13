59b60d996404de8e49193e8bc2b681c69e4c191a
jqh <841324345@qq.com>
2020-04-13 11:15:15 +0800

    update

--- src/Form.php
+++ src/Form.php
@@ -674,7 +674,7 @@ class Form implements Renderable
      *
      * @return array
      */
-    protected function removeIgnoredFields($input)
+    public function removeIgnoredFields($input)
     {
         Arr::forget($input, $this->ignored);
 
