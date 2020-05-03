8e87f477719ce0a566f3962b0b79aab7d251a98b
jqh <841324345@qq.com>
2020-05-03 13:26:14 +0800

    php7.4

--- resources/views/form/map.blade.php
+++ resources/views/form/map.blade.php
@@ -7,8 +7,8 @@
         @include('admin::form.error')
 
         <div id="map_{{$id['lat'].$id['lng']}}" style="width: 100%;height: 300px"></div>
-        <input type="hidden" id="{{$id['lat']}}" name="{{$name['lat']}}" value="{{ old($column['lat'], $value['lat']) }}" {!! $attributes !!} />
-        <input type="hidden" id="{{$id['lng']}}" name="{{$name['lng']}}" value="{{ old($column['lng'], $value['lng']) }}" {!! $attributes !!} />
+        <input type="hidden" id="{{$id['lat']}}" name="{{$name['lat']}}" value="{{ old($column['lat'], $value['lat'] ?? null) }}" {!! $attributes !!} />
+        <input type="hidden" id="{{$id['lng']}}" name="{{$name['lng']}}" value="{{ old($column['lng'], $value['lng'] ?? null) }}" {!! $attributes !!} />
 
         @include('admin::form.help-block')
 
