348266f0c39c1a5a4fecf0a62f437ed68352069c
jqh <841324345@qq.com>
2020-05-08 11:21:26 +0800

    修复菜单绑定了权限不显示问题

--- src/Models/Repositories/Menu.php
+++ src/Models/Repositories/Menu.php
@@ -6,10 +6,10 @@ use Dcat\Admin\Repositories\EloquentRepository;
 
 class Menu extends EloquentRepository
 {
-    public function __construct()
+    public function __construct($modelOrRelations = [])
     {
         $this->eloquentClass = config('admin.database.menu_model');
 
-        parent::__construct();
+        parent::__construct($modelOrRelations);
     }
 }
