1969c2f0b290d2d9c301a8cb1487f1bd029092fb
jqh <841324345@qq.com>
2020-05-08 11:19:30 +0800

    修复Displayers\Button方法报错问题

--- src/Grid/Displayers/Button.php
+++ src/Grid/Displayers/Button.php
@@ -4,7 +4,7 @@ namespace Dcat\Admin\Grid\Displayers;
 
 class Button extends AbstractDisplayer
 {
-    public function display($style)
+    public function display($style = 'primary')
     {
         $style = collect((array) $style)->map(function ($style) {
             return 'btn-'.$style;
