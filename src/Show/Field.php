7a5b16ebec768acdcea5cdd14e4b8c97a586bbef
Jiang Qinghua <841324345@qq.com>
2020-07-28 02:32:16 +0000

    Apply fixes from StyleCI

--- src/Show/Field.php
+++ src/Show/Field.php
@@ -254,7 +254,7 @@ class Field implements Renderable
     {
         $field = $this;
 
-        return $this->unescape()->as(function ($path) use ($server, $download, $field) {
+        return $this->unescape()->as(function ($path) use ($server , $field) {
             $name = basename($path);
 
             $field->wrap(false);
