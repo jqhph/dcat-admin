b03145f5dea43f13c5f1539a40c3ba3a31cd6a16
jqh <841324345@qq.com>
2020-08-23 12:08:41 +0800

    wip

--- src/Controllers/Dashboard.php
+++ src/Controllers/Dashboard.php
@@ -2,15 +2,8 @@
 
 namespace Dcat\Admin\Controllers;
 
-use Dcat\Admin\Admin;
-use Dcat\Admin\Widgets\Tab;
-use Illuminate\Support\Arr;
-
 class Dashboard
 {
-    /**
-     * @return mixed
-     */
     public static function title()
     {
         return view('admin::dashboard.title');
