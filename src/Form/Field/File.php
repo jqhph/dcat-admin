9383a32527da01fb9eb9a989aa896a28ddcb94e9
jqh <841324345@qq.com>
2020-08-27 17:37:16 +0800

    文件上传value转数组

--- src/Form/Field/File.php
+++ src/Form/Field/File.php
@@ -152,7 +152,7 @@ class File extends Field implements UploadFieldInterface
     {
         $previews = [];
 
-        foreach ($this->value() as $value) {
+        foreach (Helper::array($this->value()) as $value) {
             $previews[] = [
                 'id'   => $value,
                 'path' => basename($value),
