d0ce9463305c1afd7e8cd1f5d6c91a757c242c57
Larva Tech <46956076+larvacent@users.noreply.github.com>
2020-11-27 06:10:09 +0800

    Update embeds.blade.php

--- resources/views/form/embeds.blade.php
+++ resources/views/form/embeds.blade.php
@@ -20,4 +20,52 @@
     </div>
 </div>
 
+<hr style="margin-top: 0px;">@if($label)
+<div class="row">
+    <div class="{{$viewClass['label']}}"><h4 class="pull-right">{!! $label !!}</h4></div>
+    <div class="{{$viewClass['field']}}"></div>
+</div>
 <hr style="margin-top: 0px;">
+@endif
+
+<div id="embed-{{$column}}" class="embed-{{$column}}">
+
+    <div class="embed-{{$column}}-forms">
+
+        <div class="embed-{{$column}}-form fields-group">
+
+            @foreach($form->fields() as $field)
+                {!! $field->render() !!}
+            @endforeach
+
+        </div>
+    </div>
+</div>
+
+@if($label)
+<hr style="margin-top: 0px;">
+@endi@if($label)
+<div class="row">
+    <div class="{{$viewClass['label']}}"><h4 class="pull-right">{!! $label !!}</h4></div>
+    <div class="{{$viewClass['field']}}"></div>
+</div>
+<hr style="margin-top: 0px;">
+@endif
+
+<div id="embed-{{$column}}" class="embed-{{$column}}">
+
+    <div class="embed-{{$column}}-forms">
+
+        <div class="embed-{{$column}}-form fields-group">
+
+            @foreach($form->fields() as $field)
+                {!! $field->render() !!}
+            @endforeach
+
+        </div>
+    </div>
+</div>
+
+@if($label)
+<hr style="margin-top: 0px;">
+@endi
