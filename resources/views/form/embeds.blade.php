da522fe5bf44461fdcaf8183f7c0788842be0e9d
Larva Tech <46956076+larvacent@users.noreply.github.com>
2020-11-27 14:29:46 +0800

    Update embeds.blade.php

--- resources/views/form/embeds.blade.php
+++ resources/views/form/embeds.blade.php
@@ -1,10 +1,10 @@
-
+@if($label==false)
 <div class="row">
     <div class="{{$viewClass['label']}}"><h4 class="pull-right">{!! $label !!}</h4></div>
     <div class="{{$viewClass['field']}}"></div>
 </div>
-
 <hr style="margin-top: 0px;">
+@endif
 
 <div id="embed-{{$column}}" class="embed-{{$column}}">
 
@@ -20,4 +20,6 @@
     </div>
 </div>
 
+@if($label==false)
 <hr style="margin-top: 0px;">
+@endif
