08df386b1bc67f359ea75fc04f9653124a43df8f
卜木 <xqbumu@163.com>
2020-12-03 11:32:53 +0800

    解决 联动后原 select2 config 丢失问题
    解决 联动后原 select2 config 丢失问题，并默认在联动前置条件变化后，清空后置 options。

--- resources/views/scripts/select.blade.php
+++ resources/views/scripts/select.blade.php
@@ -44,19 +44,17 @@
         $(document).off('change', selector);
         $(document).on('change', selector, function () {
             var target = $(this).closest('{{ $load['group'] ?? '.fields-group' }}').find(".{{ $load['class'] }}");
+            target.find("option").remove();
 
             if (String(this.value) !== '0' && ! this.value) {
                 return;
             }
             $.ajax("{!! $load['url'].(strpos($load['url'],'?')?'&':'?') !!}q="+this.value).then(function (data) {
-                target.find("option").remove();
-                $(target).select2({
-                    data: $.map(data, function (d) {
-                        d.id = d.{{ $load['idField'] }};
-                        d.text = d.{{ $load['textField'] }};
-                        return d;
-                    })
-                }).val(target.attr('data-value').split(',')).trigger('change');
+                $.map(data, function (d) {
+                    var newOption = new Option(d.{{ $load['textField'] }}, d.{{ $load['idField'] }}, false, false);
+                    target.append(newOption);
+                });
+                target.val(target.attr('data-value').split(',')).trigger('change');
             });
         });
         $(selector).trigger('change');
