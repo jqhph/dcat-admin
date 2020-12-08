5fd36f84e9787f4a037f5b3e10833b0f9a5d388e
jqh <841324345@qq.com>
2020-12-08 10:07:43 +0800

    fix #797

--- resources/views/form/map.blade.php
+++ resources/views/form/map.blade.php
@@ -1,6 +1,6 @@
 <div class="{{$viewClass['form-group']}}">
 
-    <label for="{{$id['lat']}}" class="{{$viewClass['label']}} control-label">{!! $label !!}</label>
+    <label class="{{$viewClass['label']}} control-label">{!! $label !!}</label>
 
     <div class="{{$viewClass['field']}}">
 
