fa5782f185670154e2cc10821658127858a912dd
jqh <841324345@qq.com>
2021-01-05 17:37:19 +0800

    fix #938

--- resources/views/form/file.blade.php
+++ resources/views/form/file.blade.php
@@ -1,15 +1,15 @@
 <style>
     .webuploader-pick {
-        background-color: {!! admin_color()->primary() !!};
+        background-color: @primary;
     }
 
     .web-uploader .placeholder .flashTip a {
-        color: {!! admin_color()->primary(-10) !!};
+        color: @primary(-10);
     }
 
     .web-uploader .statusBar .upload-progress span.percentage,
     .web-uploader .filelist li p.upload-progress span {
-        background: {!! admin_color()->primary(-8) !!};
+        background: @primary(-8);
     }
 </style>
 
