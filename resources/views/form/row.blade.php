0a24c4355f028f14578ef808f93b17ae673ac780
jesse <jessechen@hinabian.com>
2021-01-08 18:17:17 +0800

    feat:优化Form/Row的视图

--- resources/views/form/row.blade.php
+++ resources/views/form/row.blade.php
@@ -1,7 +1,14 @@
-<div class="row" style="margin-bottom: 8px">
-    @foreach($fields as $field)
-    <div class="col-md-{{ $field['width'] }}">
-        {!! $field['element']->render() !!}
+<div class="@if(isset($options['label'])) form-group @endif row form-field">
+    @if(isset($options['width']['label']))
+    <div class="col-md-{{ $options['width']['label'] }} text-capitalize control-label">
+        <span>{{ $options['label'] }}</span>
     </div>
-    @endforeach
+    @endif
+    <div class="@if(isset($options['width']['field'])) col-md-{{ $options['width']['field'] }} @endif row">
+        @foreach($fields as $field)
+        <div class="col-md-{{ $field['width'] }}">
+            {!! $field['element']->render() !!}
+        </div>
+        @endforeach
+    </div>
 </div>
