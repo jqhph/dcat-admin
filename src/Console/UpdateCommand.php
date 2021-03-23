19a3023d5d31c99cc6c9dd5856bca7acc11d1894
jqh <841324345@qq.com>
2021-03-23 11:27:48 +0800

    2.0.21

--- src/Console/UpdateCommand.php
+++ src/Console/UpdateCommand.php
@@ -6,34 +6,18 @@ use Illuminate\Console\Command;
 
 class UpdateCommand extends Command
 {
-    /**
-     * The console command name.
-     *
-     * @var string
-     */
     protected $signature = 'admin:update';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Update the admin package';
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
     public function handle()
     {
-        try {
-            $this->call('admin:publish', ['--assets --migrations --force']);
-            $this->call('migrate');
-            $this->info('DcatAdmin has updated successfully!');
-        } catch (\Exception $exception) {
-            $this->error('Woops! Updating has failed: ' . $exception->getMessage());
-        }
-
+        $this->call('admin:publish', [
+            '--assets'     => true,
+            '--migrations' => true,
+            '--lang'       => true,
+            '--force'      => true,
+        ]);
+        $this->call('migrate');
     }
 }
