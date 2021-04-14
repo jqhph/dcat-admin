da4f756632a09001de61df7136c9f906a1983ba3
jqh <841324345@qq.com>
2021-04-14 11:21:28 +0800

    github action 所有版本

---
+++ tests/bin/install-admin.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd ./laravel-tests
+php artisan admin:publish --force
+php artisan admin:install
+php artisan migrate:rollback
+php artisan dusk:chrome-driver 89
+cp -f ./tests/routes.php ./app/Admin/
+cp -rf ./tests/resources/config ./config/
