e700f938cd66d0f415862f212705fff4849014d2
Abbotton <uctoo@foxmail.com>
2021-07-05 14:48:58 +0800

    修复类名无效的问题
    `vs-radio-success{{ $style }}`解析的最终结果是`vs-radio-successprimary`， 因此需要删除掉`success`。

--- resources/views/widgets/radio.blade.php
+++ resources/views/widgets/radio.blade.php
@@ -3,7 +3,7 @@
 @endif
 
 @foreach($options as $k => $label)
-    <div class="vs-radio-con vs-radio-success{{ $style }}" style="margin-right: {{ $right }}">
+    <div class="vs-radio-con vs-radio-{{ $style }}" style="margin-right: {{ $right }}">
         <input {!! in_array($k, $disabled) ? 'disabled' : '' !!} value="{{$k}}" {!! $attributes !!} {!! \Dcat\Admin\Support\Helper::equal($checked, $k) ? 'checked' : '' !!}>
         <span class="vs-radio vs-radio-{{ $size }}">
           <span class="vs-radio--border"></span>
