adbe64c319f0e6681c0eae0afb0e21f23d51a539
licjie <licjie@yeah.net>
2021-09-23 14:56:43 +0800

    select选项禁止

--- resources/views/grid/quick-create/multipleselect.blade.php
+++ resources/views/grid/quick-create/multipleselect.blade.php
@@ -3,7 +3,13 @@
 
         <option value=""></option>
         @foreach($options as $select => $option)
-            <option value="{{$select}}" {{ $select == old($column, $value) ?'selected':'' }}>{{$option}}</option>
+            <option value="{{$select}}" {{ $select == old($column, $value) ?'selected':'' }}
+            @if(!empty($otherOptions['disableOptions']))
+                @if((!empty($otherOptions['disableType']) && in_array($select,$otherOptions['disableOptions'])) || (empty($otherOptions['disableType']) && !in_array($select,$otherOptions['disableOptions'])))
+                    disabled
+                @endif
+            @endif
+            >{{$option}}</option>
         @endforeach
     </select>
 </div>
