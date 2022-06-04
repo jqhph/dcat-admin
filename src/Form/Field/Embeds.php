1969e72ec1cf4f8fec15bc662fefbd2d3223603d
pianzhou2021 <97172071+pianzhou2021@users.noreply.github.com>
2022-06-04 14:49:53 +0800

    upgrade embeds

--- src/Form/Field/Embeds.php
+++ src/Form/Field/Embeds.php
@@ -2,22 +2,78 @@
 
 namespace Dcat\Admin\Form\Field;
 
-use Dcat\Admin\Form\EmbeddedForm;
+use Dcat\Admin\Contracts\FieldsCollection;
+use Dcat\Admin\Form;
+use Dcat\Admin\Form\Concerns\HasFields;
 use Dcat\Admin\Form\Field;
 use Dcat\Admin\Form\ResolveField;
-use Dcat\Admin\Support\Helper;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
-
-class Embeds extends Field
+/**
+ * Class Embeds.
+ *
+ * @method Field\Text text($column, $label = '')
+ * @method Field\Checkbox checkbox($column, $label = '')
+ * @method Field\Radio radio($column, $label = '')
+ * @method Field\Select select($column, $label = '')
+ * @method Field\MultipleSelect multipleSelect($column, $label = '')
+ * @method Field\Textarea textarea($column, $label = '')
+ * @method Field\Hidden hidden($column, $label = '')
+ * @method Field\Id id($column, $label = '')
+ * @method Field\Ip ip($column, $label = '')
+ * @method Field\Url url($column, $label = '')
+ * @method Field\Email email($column, $label = '')
+ * @method Field\Mobile mobile($column, $label = '')
+ * @method Field\Slider slider($column, $label = '')
+ * @method Field\Map map($latitude, $longitude, $label = '')
+ * @method Field\Editor editor($column, $label = '')
+ * @method Field\Date date($column, $label = '')
+ * @method Field\Datetime datetime($column, $label = '')
+ * @method Field\Time time($column, $label = '')
+ * @method Field\Year year($column, $label = '')
+ * @method Field\Month month($column, $label = '')
+ * @method Field\DateRange dateRange($start, $end, $label = '')
+ * @method Field\DateTimeRange datetimeRange($start, $end, $label = '')
+ * @method Field\TimeRange timeRange($start, $end, $label = '')
+ * @method Field\Number number($column, $label = '')
+ * @method Field\Currency currency($column, $label = '')
+ * @method Field\SwitchField switch($column, $label = '')
+ * @method Field\Display display($column, $label = '')
+ * @method Field\Rate rate($column, $label = '')
+ * @method Field\Divide divider(string $title = null)
+ * @method Field\Password password($column, $label = '')
+ * @method Field\Decimal decimal($column, $label = '')
+ * @method Field\Html html($html, $label = '')
+ * @method Field\Tags tags($column, $label = '')
+ * @method Field\Icon icon($column, $label = '')
+ * @method Field\Embeds embeds($column, $label = '', Closure $callback = null)
+ * @method Field\Captcha captcha()
+ * @method Field\Listbox listbox($column, $label = '')
+ * @method Field\File file($column, $label = '')
+ * @method Field\Image image($column, $label = '')
+ * @method Field\MultipleFile multipleFile($column, $label = '')
+ * @method Field\MultipleImage multipleImage($column, $label = '')
+ * @method Field\HasMany hasMany($column, $labelOrCallback, $callback = null)
+ * @method Field\Tree tree($column, $label = '')
+ * @method Field\Table table($column, $labelOrCallback, $callback = null)
+ * @method Field\ListField list($column, $label = '')
+ * @method Field\Timezone timezone($column, $label = '')
+ * @method Field\KeyValue keyValue($column, $label = '')
+ * @method Field\Tel tel($column, $label = '')
+ * @method Field\Markdown markdown($column, $label = '')
+ * @method Field\Range range($start, $end, $label = '')
+ * @method Field\Color color($column, $label = '')
+ * @method Field\ArrayField array($column, $labelOrCallback, $callback = null)
+ * @method Field\SelectTable selectTable($column, $label = '')
+ * @method Field\MultipleSelectTable multipleSelectTable($column, $label = '')
+ * @method Field\Button button(string $html = null)
+ * @method Field\Autocomplete autocomplete($column, $label = '')
+ */
+class Embeds extends Field implements FieldsCollection
 {
     use ResolveField;
-
-    /**
-     * @var \Closure
-     */
-    protected $builder = null;
+    use HasFields;
 
     /**
      * Create a new HasMany field instance.
@@ -28,28 +84,34 @@ class Embeds extends Field
     public function __construct($column, $arguments = [])
     {
         $this->column = $column;
+        $builder    = null;
 
         if (count($arguments) == 1) {
             $this->label = $this->formatLabel();
-            $this->builder = $arguments[0];
+            $builder = $arguments[0];
         }
 
         if (count($arguments) == 2) {
-            [$this->label, $this->builder] = $arguments;
+            [$this->label, $builder] = $arguments;
+        }
+
+        if ($builder instanceof \Closure) {
+            call_user_func($builder, $this);
         }
     }
 
     /**
-     * Prepare input data for insert or update.
-     *
-     * @param  array  $input
-     * @return array
+     * @param  Form|WidgetForm  $form
+     * @return $this
      */
-    protected function prepareInputValue($input)
+    public function setForm($form = null)
     {
-        $form = $this->buildEmbeddedForm();
+        $this->form = $form;
+        foreach ($this->fields() as $field) {
+            $field->setForm($form);
+        }
 
-        return $form->setOriginal($this->original)->prepare($input);
+        return $this;
     }
 
     /**
@@ -66,7 +128,7 @@ class Embeds extends Field
         $rules = $attributes = $messages = [];
 
         /** @var Field $field */
-        foreach ($this->buildEmbeddedForm()->fields() as $field) {
+        foreach ($this->fields() as $field) {
             if (! $fieldRules = $field->getRules()) {
                 continue;
             }
@@ -224,61 +286,35 @@ class Embeds extends Field
     }
 
     /**
-     * Get data for Embedded form.
-     *
-     * Normally, data is obtained from the database.
-     *
-     * When the data validation errors, data is obtained from session flash.
-     *
-     * @return array
-     */
-    protected function getEmbeddedData()
-    {
-        return Helper::array($this->value);
-    }
-
-    /**
-     * Build a Embedded Form and fill data.
-     *
-     * @return EmbeddedForm
-     */
-    protected function buildEmbeddedForm()
-    {
-        $form = new EmbeddedForm($this->column);
-
-        $form->setParent($this->form);
-
-        $form->setResolvingFieldCallbacks($this->resolvingFieldCallbacks);
-
-        call_user_func($this->builder, $form);
-
-        $form->fill($this->getEmbeddedData());
-
-        return $form;
-    }
-
-    /**
      * Render the form.
      *
      * @return \Illuminate\View\View
      */
     public function render()
     {
-        $this->addVariables(['form' => $this->buildEmbeddedForm()]);
+        $this->addVariables(['fields' => $this->fields()]);
 
         return parent::render();
     }
 
     /**
-     * 根据字段名称查找字段.
+     * Generate a Field object and add to form builder if Field exists.
      *
-     * @param  string  $column
-     * @return Field|null
+     * @param  string  $method
+     * @param  array  $arguments
+     * @return Field
      */
-    public function findFieldByName($name)
+    public function __call($method, $arguments)
     {
-        return $this->buildEmbeddedForm()->fields()->first(function (Field $field) use ($name) {
-            return $field->column() == $name;
-        });
+        if ($className = Form::findFieldClass($method)) {
+            $column = Arr::get($arguments, 0, '');
+            $element = new $className($this->column() . '.' . $column, array_slice($arguments, 1));
+            $this->pushField($element);
+            return $element;
+        }
+
+        admin_error('Error', "Field type [$method] does not exist.");
+
+        return new Field\Nullable();
     }
 }
