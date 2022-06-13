7527041905fbfa597cf7a1d14088dde1c6d1f8f9
pianzhou2021 <97172071+pianzhou2021@users.noreply.github.com>
2022-06-13 22:14:41 +0800

    fix laravel dusk error

--- tests/bin/start.sh
+++ tests/bin/start.sh
@@ -5,4 +5,5 @@ export DISPLAY=:99.0
 #sudo Xvfb :99.0 &
 sudo chmod -R 0755 ./vendor/laravel/dusk/bin/
 #./vendor/laravel/dusk/bin/chromedriver-linux  > /dev/null 2>&1 &
+php artisan laravel:dusk > /dev/null 2>&1 &
 php artisan serve --port=8300 > /dev/null 2>&1 &
