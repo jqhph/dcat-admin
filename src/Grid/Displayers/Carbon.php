b1ca0ed66c48e875e85b117f1fd98043638992e9
Flc゛ <four_leaf_clover@foxmail.com>
2022-09-14 12:25:20 +0800

    fixed code

--- src/Grid/Displayers/Carbon.php
+++ src/Grid/Displayers/Carbon.php
@@ -8,25 +8,27 @@ use Carbon\CarbonInterface;
  * Carbon 日期格式化.
  *
  * @example $grid->column('created_at')->carbon();
- *          $grid->column('created_at')->carbon('Y-m-d H:i:s');
+ *          $grid->column('created_at')->carbon('Y-m-d H:i:s', '-');
  *          $grid->column('created_at')->carbon(fn ($value) => $value->format('Y-m-d'));
  */
 class Carbon extends AbstractDisplayer
 {
     /**
-     * @param  \Closure|string  $format
-     * @return string
+     * @param  string|\Closure  $format
+     * @param  mixed            $default
+     *
+     * @return string|null
      */
-    public function display(string|\Closure $format = 'Y-m-d H:i:s'): string
+    public function display(string|\Closure $format = 'Y-m-d H:i:s', mixed $default = ''): ?string
     {
         if (! $this->value instanceof CarbonInterface) {
-            throw new \InvalidArgumentException('The value must be an instance of CarbonInterface');
+            return $default;
         }
 
         if ($format instanceof \Closure) {
             return $format($this->value);
         }
 
-        return $this->value->format($format);
+        return $this->value?->format($format) ?? $default;
     }
 }
